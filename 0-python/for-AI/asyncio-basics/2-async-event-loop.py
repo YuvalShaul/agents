003e91c36bb10fd016568522ca73c49d1e7a9094
Yuval Shaul <yuval.shaul@gmail.com>
2026-02-08 09:29:01 +0200

    updating the loop example

--- 0-python/for-AI/asyncio-basics/2-async-event-loop.py
+++ 0-python/for-AI/asyncio-basics/2-async-event-loop.py
@@ -7,8 +7,12 @@ async def say(what, when):
     await asyncio.sleep(when)
     print(what)
 
+# I could call get_event_loop() and get the same event loop, but this behaviour may change in the future.
+# # loop = asyncio.get_event_loop()
+# The get_event_loop() function will create an event loop, but only for the main thread, and this behaviour may change in the future.
 
-loop = asyncio.get_event_loop()
+# But I could also create a new event loop
+loop = asyncio.new_event_loop()
 print(loop)
 loop.run_until_complete(say('hello world', 1))
 loop.close()
